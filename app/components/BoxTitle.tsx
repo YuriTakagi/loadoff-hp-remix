730b1ddcbb9eaab82c940c839572cdd26988da66
YuriTakagi <takagi@loadoff.jp>
2024-07-01 13:31:13 +0900

    BoxTitleCompを作成

---
+++ app/components/BoxTitle.tsx
@@ -0,0 +1,8 @@
+export default function BoxTitle() {
+  return (
+    <div className="bg-white w-1/3 p-6 rounded-lg">
+      <div className="text-3xl">Falling boxes</div>
+      <p>Three.js</p>
+    </div>
+  );
+};
